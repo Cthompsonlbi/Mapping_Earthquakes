f8d4ce9bfec5b2a1d6647ab2d15eacad5a5f595f
Chad Thompson <Chad.Thompson@Qorvo.com>
2022-01-19 13:56:20 -0500

    multiple points/city

---
+++ Mapping_Multiple_Points/static/js/cities.js
@@ -0,0 +1,55 @@
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  },
+  {
+    location: [4.710989, -74.072090],
+    city: "Bogota",
+    state: "CO",
+    population: 7410000
+  },
+  {
+    location: [42.740219, -78.132591],
+    city: "Warsaw",
+    state: "NY",
+    population: 3473
+  },
+  {
+    location: [9.930169, -84.077531],
+    city: "San Jose",
+    state: "CR",
+    population: 342188
+  },
+  {
+    location: [27.273270, -80.342148],
+    city: "Port Saint Lucie",
+    state: "FL",
+    population: 204851
+  }
+  ];
